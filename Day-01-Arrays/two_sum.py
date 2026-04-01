b74d197d57338dce28e5f70c95fc58f8532d4a7c
Chandangowda <VGCHANDANGOWDA@GMAIL.COM>
2026-04-01 16:51:11 +0530

    Day 1: solved two sum using hashmap (0(n))

--- Day-01-Arrays/two_sum.py
+++ Day-01-Arrays/two_sum.py
@@ -1,1 +1,18 @@
-print("Day 1 - DSA Started 🚀")
+def twoSum(nums, target):
+    hashmap = {}
+
+    for i in range(len(nums)):
+        complement = target - nums[i]
+
+        if complement in hashmap:
+            return [hashmap[complement], i]
+
+        # FIXED: correct variable + correct indentation
+        hashmap[nums[i]] = i
+
+
+# test
+nums = [2, 7, 11, 15]
+target = 9
+
+print(twoSum(nums, target))
